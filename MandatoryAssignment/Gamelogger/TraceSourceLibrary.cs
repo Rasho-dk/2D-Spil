8bab4b8c3249020212cd692386df8cd6c3e9bd48
Rasho-dk <103439939+Rasho-dk@users.noreply.github.com>
2024-04-09 12:37:02 +0200

    aflevering

--- MandatoryAssignment/Gamelogger/TraceSourceLibrary.cs
+++ MandatoryAssignment/Gamelogger/TraceSourceLibrary.cs
@@ -5,6 +5,8 @@ namespace MandatoryAssignment.Gamelogger
 {
     public class TraceSourceLibrary
     {
+
+
         #region Singleton
         /// This class is a singleton class, which means that there can only be one instance of the class.
         /// </summary>
@@ -15,7 +17,7 @@ namespace MandatoryAssignment.Gamelogger
 
         //private GameConfiguration GameConfiguration { get; set; }
 
-        /// This object is used to create a trace source, which is used to log events in the game.
+        /// This object is used to create a trace source, which is used to log events in the game. 
         private TraceSource traceSource;
 
         /// <summary>
